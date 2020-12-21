f2f8c3d7da8787ec65a246bbd84e3b90d9d939bc
fukugit <fukuma1234@gmail.com>
2020-12-21 20:13:15 +0900

    First commit

---
+++ cli-vue/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+    <MyClient/>
+  </div>
+</template>
+
+<script>
+// import HelloWorld from './components/HelloWorld.vue'
+import MyClient from './components/MyClient.vue'
+
+export default {
+  name: 'App',
+  components: {
+    MyClient
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
